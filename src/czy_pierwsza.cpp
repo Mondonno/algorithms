735d62e1af477333894537cb919aefa0d75285dd
Mondonno <mikolaj.mocek@icloud.com>
2021-08-23 20:52:50 +0200

    feat: Added checking that the number is firstable, and addded eratostenes algorithm

---
+++ src/czy_pierwsza.cpp
@@ -0,0 +1,9 @@
+int sprawdz(int x) 
+{
+  if(x == 1) return false;
+  for(int i = 2; i < sqrt(x); i++) {
+    if(x % i != 0) return false;
+  }
+
+  return true;
+}
---
+++ src/sito_eratostenesa.cpp
@@ -0,0 +1,16 @@
+// algorytm określajacy pierwszosc liczby do jakiegos N
+
+int* sito(int n) {
+   int liczby_pierwsze[n + 1];
+
+   liczby_pierwsze[0] = 1;
+   liczby_pierwsze[1] = 1;
+   for(int i = 2; i < n+1; i++) {
+      if(liczby_pierwsze[i] == 1) continue;
+      for(int j = i * i; j < n; i++) {
+         liczby_pierwsze[j] = 1;
+      }
+   }
+
+   return liczby_pierwsze;
+}
