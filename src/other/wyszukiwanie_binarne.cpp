613fc75cdc4a8683bd82552258ae5cb25f6e0d84
Mondonno <mikolaj.mocek@icloud.com>
2021-09-17 21:47:11 +0200

    fix: Fixed bin-search

--- src/other/wyszukiwanie_binarne.cpp
+++ src/other/wyszukiwanie_binarne.cpp
@@ -1,32 +1,14 @@
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    // dodany fragment
-    int n;
-    cin >> n;
+int binSearch(int poczatek, int koniec, int szukane, vector<int> tablica) {
+    while(poczatek <= koniec){
+        int srodek = (poczatek + koniec) / 2;
 
-    int tablica[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> tablica[i];
-    }
-
-    int x = 0;
-    // koniec dodanego fragmentu
-    
-    int poczatek = 0;
-    int koniec = n - 1;
-    while (poczatek < koniec)
-    {
-        // to jest dzielenie przez 2 z zaokrągleniem w górę
-        // czyli ceil bez ceila, dla tych ktorzy nie sa zorientowani
-        int srodek = (poczatek + koniec + 1) / 2; 
-        if (tablica[srodek] <= x)
-            poczatek = srodek;
-        else
+        if(tablica[srodek] < szukane) {
+            poczatek = srodek + 1;
+        }else
             koniec = srodek - 1;
     }
 }
