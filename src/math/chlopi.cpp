ae64c92b2897308ccaed1ce730e5b988e251da39
Mondonno <mikolaj.mocek@icloud.com>
2021-10-05 20:23:55 +0200

    feat: Added new russian peasant multiply algorithm

---
+++ src/math/chlopi.cpp
@@ -0,0 +1,8 @@
+// metoda mnozenia metoda chlopow rosyjskich
+
+int mnozenieChlopskie(int a, int b) {
+    if (b == 0) return 0;
+    int h = mnozenieChlopskie(a, b / 2) * 2;
+    if (b % 2 == 0) return h;
+    return h + a;
+}
