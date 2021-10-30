aef73c728bb0a0ccc514a0e5eda9e9763e96f5cb
Mondonno <mikolaj.mocek@icloud.com>
2021-10-30 21:34:22 +0200

    feat, fix: Erastostenes algorithm was fixed and works now also added a note how to use the returned data from it

--- src/math/sito_eratostenesa.cpp
+++ src/math/sito_eratostenesa.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 // algorytm określajacy pierwszosc liczby do jakiegos N
+// wszystkie indeksy okreslone jako falsz sa liczbami pierwszymi
 
-vector<int> sito(int n) {
-   vector<int> liczby_pierwsze(n+1);
+vector<bool> sito(int n)
+{
+   vector<bool> pierwsze(n + 1, false);
+   pierwsze[0] = 1;
+   pierwsze[1] = 1;
 
-   liczby_pierwsze[0] = 1;
-   liczby_pierwsze[1] = 1;
-   
-   for(int i = 2; i < n+1; i++) {
-      if(liczby_pierwsze[i] == 1) continue;
-      for(int j = i * i; j < n; i++) {
-         liczby_pierwsze[j] = 1;
+   for (int i = 2; i * i <= n; i++)
+   {
+      if (pierwsze[i])
+         continue;
+      for (int j = i * i; j <= n; j += i)
+      {
+         pierwsze[j] = 1;
       }
    }
-
-   return liczby_pierwsze;
+   return pierwsze;
 }
